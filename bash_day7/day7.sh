c8be212db0d24b17004949aecf81acaf850a4959
Jacquefresco1 <h3yJacqueFresco@gmail.com>
2026-08-26 18:33:34 +0000

    Bash, потихоньку

--- bash_day7/day7.sh
+++ bash_day7/day7.sh
@@ -1,25 +1,52 @@
-name="Jacque Fresco"
-current_dir=$(pwd)
-today=$(date)
+#!/bin/bash
 
-printf '<%s>\n' "$name"
-printf '<%s>\n' $name
-echo "$current_dir"
-echo "Я сейчас нахожусь здесь: $(pwd)"
-echo "Сегодня: $today"
-echo "hello"
-echo $?
-ls
-echo $?
-ls /this-does-not-exist
+##Variables
+name="Alex Smith" 
+echo "$name" 
+echo '$name'
+
+##Command substitution
+today=$(date) 
+echo "Today: $today" 
+echo "Current date: $(date)"
+
+##Exit status
+echo "hello" 
 echo $?
 
-ls
-if [ $? -eq 0 ]; then
-	echo "Команда успешна"
+##Conditions
+if [ -f test.txt ]; then 
+	echo "test file exists" 
+else 
+	echo "test file does not exist" 
 fi
 
-ls /this-does-not-exist
-if [ $? -eq 0 ]; then
-	echo "Команда успешна"
+if [ -d backup ]; then 
+	echo "backup directory exists" 
+else 
+	echo "backup directory does not exist" 
 fi
+
+##AND(&&) / OR(||) 
+if [ -d backup ] && [ -f test.txt ]; then 
+	echo "directory and file exists" 
+else 
+	echo "directory or file does not exist" 
+fi
+
+##Arguments
+echo "Script: $0" 
+echo "First: $1" 
+echo "Second: $2" 
+echo "Third: $3" 
+echo "Number of arguments: $#" 
+echo "All arguments: $@"
+
+##Loop through arguments
+for arg in "$@"; do 
+	if [ -f "$arg" ]; then 
+		echo "$arg exists" 
+	else 
+		echo "$arg does not exist" 
+	fi 
+done
---
+++ bash_day7/test2.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+echo "Script: $0"
+echo "First arguments: $1"
+echo "Second arguments: $2"
+echo "Third arguments: $3"
+echo "Number of arguments: $#"
+echo "All argements: $@"
